a5230e0233510412c59bf786b9906da7c80dc448
musfiqurofficial <musfiqurofficial1999@gmail.com>
2022-10-28 11:30:47 +0600

    change

--- src/components/Pages/Course.jsx
+++ src/components/Pages/Course.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Course = ({ course }) => {
-    const { _id,  category_id, details, name, thumbnail_url, title, rating } = course;
+    const { _id, details, name, thumbnail_url, title, rating } = course;
 
     return (
         <div>
